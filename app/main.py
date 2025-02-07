755ab65b551cd396846186700b6bcf227472381f
npd.yana.maksimova <yana.maksimova@restream.ru>
2025-02-08 02:53:25 +0300

    lesson 4

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+import logging
+from contextlib import asynccontextmanager
+
 import dotenv
 
 dotenv.load_dotenv()
@@ -6,11 +9,20 @@ import uvicorn
 
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
-from routers import status, users
+from app.routers import status, users
 from app.database.engine import create_db_and_tables
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logging.warning("On startup")
+    create_db_and_tables()
+    yield
+
+    logging.warning("On shutdown")
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(status.router)
 app.include_router(users.router)
 add_pagination(app)
